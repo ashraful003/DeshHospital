864ab353c53d43f5daa896b95add222a51d63e7d
ashraful003 <ashraful10178@gmail.com>
2025-05-18 14:57:31 +0600

    Create Desh Hospital Project AA DH-1000

---
+++ app/src/main/java/com/myapp/deshhospital/presentation/dashboard/adapter/ViewPageAdapter.kt
@@ -0,0 +1,26 @@
+package com.myapp.deshhospital.presentation.dashboard.adapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.myapp.deshhospital.R
+
+class ViewPageAdapter(private val views:List<View>):RecyclerView.Adapter<ViewPageAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val container:FrameLayout = itemView.findViewById(R.id.view_container)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.loding_landing_items,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return views.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.container.addView(views[position])
+    }
+}
